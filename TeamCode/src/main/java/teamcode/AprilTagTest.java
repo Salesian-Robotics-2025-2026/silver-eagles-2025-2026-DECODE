3923d7e91fd1b7110e4cf521fb08a54e96aae278
ryan <ryan.malone@salesianhigh.org>
2025-12-04 21:14:26 -0500

    finished apriltag proof of concept i think

--- TeamCode/src/main/java/teamcode/AprilTagTest.java
+++ TeamCode/src/main/java/teamcode/AprilTagTest.java
@@ -3,6 +3,8 @@ package teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
 import teamcode.mechanisms.AprilTagWebcam;
 
 @TeleOp(name = "AprilTagTest", group = "TeleOp")
@@ -12,11 +14,14 @@ public class AprilTagTest extends OpMode {
 
     @Override
     public void init() {
-
+        aprilTagWebcam.init(hardwareMap, telemetry);
     }
 
     @Override
     public void loop() {
+        aprilTagWebcam.update();
+        AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificId(20);
+        telemetry.addData("id20 string", id20.toString());
     }
 
 }
